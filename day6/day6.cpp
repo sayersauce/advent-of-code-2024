3502916e9476317f98d98264411b9c1c80809982
Max Sayer <sayer_max@yahoo.co.uk>
2024-12-06 18:39:02 +0000

    day 6 part 2

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -6,67 +6,13 @@
 #include <set>
 
 using namespace std;
+using coord = pair<int, int>; // i, j coordinates on grid
+using location = pair<coord, char>; // i, j coordinates with the guard's direction ^,>,v,<
 
-int main() {
-    ifstream file("input.txt");
-    string line;
-    vector<vector<char>> grid;
-    vector<vector<int>> obstructions;
-    vector<int> guard;
-
-    int i = 0;
-
-    while (getline(file, line)) {
-        int j = 0;
-        vector<char> row;
-
-        for (char c : line) {
-            row.push_back(c);
-
-            if (c == '^') {
-                guard = {i, j};
-            } else if (c == '#') {
-                obstructions.push_back({i , j});
-            }
-
-            j++;
-        }
-
-        grid.push_back(row);
-
-        i++;
-    }
-
-    // debug
-    if (true) {
-        int n = grid[0].size();
-
-        cout << " ";
-
-        for (int i = 0; i < n; i++) {
-            cout << i;
-        }
-
-        cout << endl;
-
-        int i = 0;
-
-        for (auto row : grid) {
-            cout << i;
-            for (auto c : row) {
-                cout << c;
-            }
-            cout << endl;
-            i++;
-        }
-
-        cout << "guard " << guard[0] << ", " << guard[1] << endl;
-    }
-
-    // The grid is a square
+set<coord> part1(vector<vector<char>> grid, vector<int> guard) {
     int n = grid[0].size();
     char dir = '^';
-    set<pair<int, int>> visited;
+    set<coord> visited;
 
     while (guard[0] < n && guard[0] >= 0 && guard[1] < n && guard[1] >= 0) {
         visited.insert(make_pair(guard[0], guard[1]));
@@ -96,7 +42,7 @@ int main() {
                 guard[0]++;
             }
         } else {
-            if (guard[1] + -1 == -1) {
+            if (guard[1] - 1 == -1) {
                 guard[1]--;
             } else if (grid[guard[0]][guard[1] - 1] == '#') {
                 dir = '^';
@@ -106,7 +52,109 @@ int main() {
         }
     }
 
+    return visited;
+}
+
+bool checkIfLoop(vector<vector<char>> grid, vector<int> guard) {
+    int n = grid[0].size();
+    char dir = '^';
+
+    set<location> visited;
+
+    while (guard[0] < n && guard[0] >= 0 && guard[1] < n && guard[1] >= 0) {
+        location loc = make_pair(make_pair(guard[0], guard[1]), dir);
+
+        if (visited.find(loc) != visited.end()) {
+            return true;
+        }
+
+        visited.insert(loc);
+
+        if (dir == '^') {
+            if (guard[0] - 1 == -1) {
+                guard[0]--;
+            } else if (grid[guard[0] - 1][guard[1]] == '#') {
+                dir = '>';
+            } else {
+                guard[0]--;
+            }
+        } else if (dir == '>') {
+            if (guard[1] + 1 == n) {
+                guard[1]++;
+            } else if (grid[guard[0]][guard[1] + 1] == '#') {
+                dir = 'v';
+            } else {
+                guard[1]++;
+            }
+        } else if (dir == 'v') {
+            if (guard[0] + 1 == n) {
+                guard[0]++;
+            } else if (grid[guard[0] + 1][guard[1]] == '#') {
+                dir = '<';
+            } else {
+                guard[0]++;
+            }
+        } else {
+            if (guard[1] - 1 == -1) {
+                guard[1]--;
+            } else if (grid[guard[0]][guard[1] - 1] == '#') {
+                dir = '^';
+            } else {
+                guard[1]--;
+            }
+        }
+    }
+
+    return false;
+}
+
+int part2(vector<vector<char>> grid, vector<int> guard, set<pair<int, int>> path) {
+    // Brute force.
+    // Originally looped through all cells, optimised slightly by only checking the original path of the guard.
+    // From ~ 60 seconds to ~ 15 seconds after this change, for this input data.
+    int n = grid[0].size();
+    int loops = 0;
+
+    for (auto [i, j] : path) {
+        vector<vector<char>> newGrid = grid;
+        newGrid[i][j] = '#';
+        loops += checkIfLoop(newGrid, guard);
+    }
+
+    return loops;
+}
+
+int main() {
+    ifstream file("input.txt");
+    string line;
+    vector<vector<char>> grid;
+    vector<int> guard;
+
+    int i = 0;
+
+    while (getline(file, line)) {
+        int j = 0;
+        vector<char> row;
+
+        for (char c : line) {
+            row.push_back(c);
+
+            if (c == '^') {
+                guard = {i, j};
+            }
+
+            j++;
+        }
+
+        grid.push_back(row);
+
+        i++;
+    }
+
+    set<coord> visited = part1(grid, guard);
+
     cout << visited.size() << endl;
+    cout << part2(grid, guard, visited) << endl;
     
     return 0;
 }
