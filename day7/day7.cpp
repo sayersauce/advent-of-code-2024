1ac8bf1f04b5d18afce84414e2400ffb59be13eb
Max Sayer <sayer_max@yahoo.co.uk>
2024-12-08 00:47:55 +0000

    day 7

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <map>
 #include <set>
+#include <utility>
 
 using namespace std;
 
@@ -21,79 +22,14 @@ void cartesianProduct(const vector<char>& set, int size, vector<char>& current, 
     }
 }
 
-int main() {
-    ifstream file("input.txt");
-    string line;
-    map<long long, vector<int>> equations;
-    set<int> equationLengths;
+long part1(vector<pair<long, vector<int>>> equations, map<int, vector<vector<char>>> operatorCombinations) {
+    long result = 0;
 
-    while (getline(file, line)) {
-        istringstream ss(line);
-        string s;
-        bool first = true;
-        long long total;
-        vector<int> numbers;
-
-        while (ss >> s) {
-            if (first) {
-                total = stoll(s.substr(0, s.size() - 1));
-                first = false;
-            } else {
-                numbers.push_back(stoll(s));
-            }
-        }
-    
-        equations[total] = numbers;
-        equationLengths.insert(numbers.size());
-    }
-
-    // debug
-    for (auto [key, vals] : equations) {
-        cout << key << ": ";
-
-        for (int val : vals) {
-            cout << val << ", ";
-        }
-
-        cout << endl;
-    }
-
-    map<int, vector<vector<char>>> operatorCombinations;
-
-    for (int length : equationLengths) {
-        cout << "equation len " << length << endl; // debug
-        vector<char> ops = {'*', '+'};
-
-        vector<vector<char>> combinations;
-        vector<char> current;
-
-        cartesianProduct(ops, length - 1, current, combinations);
-
-        operatorCombinations[length] = combinations;
-    }
-
-
-// debug
-    // for (auto [key, vals] : operatorCombinations) {
-    //     cout << key << endl;
-
-    //     for (vector<char> v : vals) {
-    //         for (char c : v) {
-    //             cout << c << " ";
-    //         }
-    //         cout << endl;
-    //     }
-    // }
-
-    long long part1 = 0;
- 
     for (auto [target, numbers] : equations) {
-        // cout << endl << "processing equation " << target << endl;
-
         vector<vector<char>> combinations = operatorCombinations[numbers.size()];
 
         for (vector<char> combination : combinations) {
-            long long total = numbers[0];
+            long total = numbers[0];
             int i = 1;
 
             while (i < numbers.size()) {
@@ -112,13 +48,94 @@ int main() {
             }
 
             if (total == target) {
-                // cout << target << " this equation works" << endl;
-                part1 += target;
+                result += target;
                 break;
             }
         }
     }
 
-    cout << part1 << endl;
+    return result;
+}
+
+long part2(vector<pair<long, vector<int>>> equations, map<int, vector<vector<char>>> operatorCombinations) {
+    long result = 0;
+
+    for (auto [target, numbers] : equations) {
+        vector<vector<char>> combinations = operatorCombinations[numbers.size()];
+
+        for (vector<char> combination : combinations) {
+            long total = numbers[0];
+            int i = 1;
+
+            while (i < numbers.size()) {
+                char op = combination[i - 1];
+                if (total > target) {
+                    break;
+                }
+
+                if (op == '+') {
+                    total += numbers[i];
+                } else if (op == '*') {
+                    total *= numbers[i];
+                } else if (op == '|') {
+                    total = stol(to_string(total) + to_string(numbers[i]));
+                }
+                
+                i++;
+            }
+
+            if (total == target) {
+                result += target;
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    ifstream file("input.txt");
+    string line;
+    vector<pair<long, vector<int>>> equations;
+    set<int> equationLengths;
+
+    while (getline(file, line)) {
+        istringstream ss(line);
+        string s;
+        bool first = true;
+        long total;
+        vector<int> numbers;
+
+        while (ss >> s) {
+            if (first) {
+                total = stol(s.substr(0, s.size() - 1));
+                first = false;
+            } else {
+                numbers.push_back(stol(s));
+            }
+        }
     
+        equations.push_back(make_pair(total, numbers));
+        equationLengths.insert(numbers.size());
+    }
+
+    vector<char> ops = {'*', '+'};
+    vector<char> ops2 = {'*', '+', '|'};
+    map<int, vector<vector<char>>> operatorCombinations;
+    map<int, vector<vector<char>>> operatorCombinations2;
+
+    for (int length : equationLengths) {
+        vector<vector<char>> combinations, combinations2;
+        vector<char> current, current2;
+
+        cartesianProduct(ops, length - 1, current, combinations);
+        cartesianProduct(ops2, length - 1, current2, combinations2);
+
+        operatorCombinations[length] = combinations;
+        operatorCombinations2[length] = combinations2;
+    }
+
+    cout << part1(equations, operatorCombinations) << endl;
+    cout << part2(equations, operatorCombinations2) << endl;
 }
